0755089f606429b92ced6e778f7c1d9969e15539
f04k04369 <curb369@gmail.com>
2026-02-11 23:39:36 +0900

    カードレイアウト調整

--- src/components/restaurant-card.tsx
+++ src/components/restaurant-card.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import Image from 'next/image'
+import { Heart } from 'lucide-react'
+import Link from 'next/link'  
 
 export default function RestaurantCard() {
   return (
-    <div>
-      RestaurantCard
+    <div className='relative'>
+      <Link href={"/abc"} className='inset-0 absolute z-10'></Link>
+      <div className='relative aspect-video rounded-lg overflow-hidden'>
+        <Image className='object-cover' src={"/test-img.png"} fill alt="レストラン画像" />
+      </div>
+      <div className='flex justify-between items-center'>
+        <p className='font-bold'>name</p>
+        <div className='z-20'>
+          <Heart color='grey' strokeWidth={3} size={15} className='hover:fill-red-500 hover:stroke-0'/>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
