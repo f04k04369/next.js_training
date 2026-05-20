ad5eff753d7e1221a95fd27226ea5bdbfcd4d869
kosukefujisawa <curb369@gmail.com>
2026-05-20 23:25:04 +0900

    68_サジェスチョンを表示しよう

--- src/components/place-search-bar.tsx
+++ src/components/place-search-bar.tsx
@@ -10,19 +10,27 @@ import {
   CommandItem,
   CommandList,
 } from "@/components/ui/command";
+import { RestaurantSuggestion } from "@/types";
+import { MapPinIcon, SearchIcon } from "lucide-react";
 
 export default function PlaceSearchBar() {
   const [open, setOpen] = useState(false);
   const [inputText, setInputText] = useState("");
   const [sessionToken, setSessionToken] = useState(uuidv4());
-  const fetchSuggestions = useDebouncedCallback(async (input:string) => {
+  const [suggestions, setSuggestions] = useState<RestaurantSuggestion[]>([]);
+  const fetchSuggestions = useDebouncedCallback(async (input: string) => {
     console.log(input);
     try {
-      const response = await fetch(`/api/restaurant/autocomplete?input=${input}&sessionToken=${sessionToken}`);
+      const response = await fetch(
+        `/api/restaurant/autocomplete?input=${input}&sessionToken=${sessionToken}`,
+      );
+      const data: RestaurantSuggestion[] = await response.json();
+      console.log("data", data);
+      setSuggestions(data);
     } catch (error) {
       console.error(error);
     }
-  },500);
+  }, 500);
   useEffect(() => {
     if (!inputText.trim()) {
       setOpen(false);
@@ -54,9 +62,27 @@ export default function PlaceSearchBar() {
         <div className="relative">
           <CommandList className="absolute bg-background w-full shadow-md rounded-lg">
             <CommandEmpty>No results found.</CommandEmpty>
-            <CommandItem>Calendar</CommandItem>
-            <CommandItem>Search Emoji</CommandItem>
-            <CommandItem>Calculator</CommandItem>
+            {suggestions.map((suggestion) => {
+              const label =
+                suggestion.type === "placePrediction"
+                  ? suggestion.placeName
+                  : suggestion.query;
+              const value =
+                suggestion.type === "placePrediction"
+                  ? suggestion.placeId
+                  : suggestion.query;
+
+              return (
+                <CommandItem key={value} value={value} className="padding-5">
+                  {suggestion.type === "queryPrediction" ? (
+                    <SearchIcon />
+                  ) : (
+                    <MapPinIcon />
+                  )}
+                  <p>{label}</p>
+                </CommandItem>
+              );
+            })}
           </CommandList>
         </div>
       )}
